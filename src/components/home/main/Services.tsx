91ee61e02ec5da3ae9ca6b593fb2852ab1ac89f9
Stas <ssfrontendd@gmail.com>
2024-03-27 15:37:17 +0100

    first commit

---
+++ src/components/home/main/Services.tsx
@@ -0,0 +1,32 @@
+export default function Services() {
+  return (
+    <section className="bg-zinc-800 w-full h-screen text-white">
+      <div className="container">Services</div>
+    </section>
+  );
+}
+
+/*
+<section className="text-gray-400 bg-zinc-800 body-font min-h-screen">
+<div className=" px-5 py-24 mx-auto">
+  <div className="flex flex-wrap -m-4">
+    <div className="lg:w-1/3 md:w-1/2 p-4 w-full">
+      <a className="block relative h-48 rounded overflow-hidden">
+        <img
+          alt="ecommerce"
+          className="object-cover object-center w-full h-full block"
+          src="https://dummyimage.com/420x260"
+        />
+      </a>
+      <div className="mt-4">
+        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+          CATEGORY
+        </h3>
+        <h2 className="text-white title-font text-lg font-medium">
+          The Catalyzer
+        </h2>
+        <p className="mt-1">$16.00</p>
+      </div>
+    </div>
+
+    */
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,19 @@
 import type { Config } from "tailwindcss";
 
+/*
+ЦВЕТ ФОНА/ТЕКСТА СТРАНИЦЫ:
+
+- Главный цвет фона сайта: #E8E8E8
+- Цвет поля: #2E2D2D
+
+Цвет шрифта:
+
+1. Бежевый - #E8E8E8
+2. Бежевый - #D6D3CA ( Project ) 
+3. Темный - #2E2D2D
+4. Зелёный - #56503F ( Options. )
+*/
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,11 +21,21 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      // padding: "2rem",
+    },
     extend: {
+      colors: {
+        mainBg: "#E8E8E8",
+        fieldColor: "#2E2D2D",
+        fontBeigeOne: "#E8E8E8",
+        fontBeigeSec: "#D6D3CA",
+        fontDark: "#2E2D2D",
+        fontGreen: "#56503F",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero-bg": "url('/images/test1.png')",
       },
     },
   },
